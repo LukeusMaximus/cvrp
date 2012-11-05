93c8b87f899215dd6354962c8d476999107b97ab
Lukeus Maximus <github@lukemurray.co.uk>
2012-11-05 16:45:42 +0000

    Created fitness function

--- cvrp.py
+++ cvrp.py
@@ -1,12 +1,6 @@
 import sys
+import math
 from random import shuffle
-
-class node:
-    def __init__(self, ID, x, y, d):
-        self.ID = ID
-        self.x = x
-        self.y = y
-        self.d = d
 
 class cvrp_solver:
     def __init__(self, nodes, depot_id, capacity):
@@ -14,13 +8,38 @@ class cvrp_solver:
         self.dimension = len(nodes)
         self.depot_id = depot_id
         self.capacity = capacity
+    def __distance(self, id1, id2):
+        return math.sqrt((self.nodes[id1][0] - self.nodes[id2][0])**2 + (self.nodes[id1][1] - self.nodes[id2][1])**2)
     def initialise_population(self, size):
-        self.population = []
+        genes = self.nodes.keys()
+        genes.remove(depot_id)
+        self.population = {}
         for i in xrange(size):
-            genome = [x for x in xrange(2, self.dimension)]
-            shuffle(genome)
-            self.population.append(genome)
-            print genome
+            dna = genes
+            shuffle(dna)
+            self.population[i] = (dna, 0)
+            print self.population[i]
+    def calculate_fitnesses(self):
+        for x in self.population.keys():
+            genome = self.population[x][0]
+            cap = self.capacity
+            cost = self.__distance(depot_id, genome[0])
+            cap -= self.nodes[genome[0]][2]
+            for i in xrange(len(genome)-1):
+                if cap - self.nodes[genome[i+1]][2] < 0:
+                    cost += self.__distance(genome[i], depot_id)
+                    cap = self.capacity
+                    cost += self.__distance(depot_id, genome[i+1])
+                else:
+                    cost += self.__distance(genome[i], genome[i+1])
+                cap -= self.nodes[genome[i+1]][2]
+            self.population[x] = genome, cost
+        print self.population
+    def crossover(self):
+        #Also selects parents
+        pass
+    def mutate(self):
+        pass    
 
 def parse_file(filename):
     cvrp_file = open(sys.argv[1])
@@ -48,7 +67,7 @@ def parse_file(filename):
         coord_line_vals = [int(x) for x in lines[coord_sec + i].split(" ")]
         dem_line_vals = [int(x) for x in lines[dem_sec + i].split(" ")]
         assert coord_line_vals[0] == dem_line_vals[0]
-        nodes[coord_line_vals[0]] = node(coord_line_vals[0], coord_line_vals[1], coord_line_vals[2], dem_line_vals[1])
+        nodes[coord_line_vals[0]] = (coord_line_vals[1], coord_line_vals[2], dem_line_vals[1])
     depot_id = int(lines[dep_sec+1])
     return nodes, depot_id, capacity
 
@@ -57,6 +76,7 @@ if __name__ == "__main__":
         nodes, depot_id, capacity = parse_file(sys.argv[1])
         solver = cvrp_solver(nodes, depot_id, capacity)
         solver.initialise_population(1)
+        solver.calculate_fitnesses()
     else:
         print "Incorrcect number of arguments.\nUsage: \"python cvrp.py <vrp_file>\""
 
