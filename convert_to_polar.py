4d97c4b1eac93b47141aeac6e4db49d7ae9cb3e1
Lukeus Maximus <lm9131@bristol.ac.uk>
2012-11-20 16:47:41 +0000

    Added polar coordinate converter.

---
+++ convert_to_polar.py
@@ -0,0 +1,13 @@
+import math
+from coordinates import coordinates
+
+if __name__ == "__main__":
+    polar = {}
+    for i in xrange(2, 77):
+        angle = 0
+        if coordinates[i]["x"] - coordinates[i]["y"] == 0:
+            if coordinates[i]["y"] > 0:
+                angle = math.
+            else:
+        math.tan()
+
